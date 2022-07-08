49c681a1db6f258fb9e55ed2d6c7bdb47c6bd4f9
zaid.khan <zaid.khan@10pearls.com>
2022-07-08 19:09:46 +0500

    initialized empty project

---
+++ src/main/java/com/wego/parking/WegoParkingApplication.java
@@ -0,0 +1,13 @@
+package com.wego.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegoParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WegoParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wego/parking/WegoParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wego.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegoParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
